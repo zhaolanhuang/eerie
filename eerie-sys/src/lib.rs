2f501d4e106f9f18cb7fb58c135d9acbbd424b75
Zhaolan Huang <zhaolan.huang@fu-berlin.de>
2025-09-05 16:08:51 +0000

    uncomment _fini, need to reconize native vs non-native, which is both no_std

--- eerie-sys/src/lib.rs
+++ eerie-sys/src/lib.rs
@@ -5,12 +5,13 @@ pub mod compiler;
 #[cfg(feature = "runtime")]
 pub mod runtime;
 
-// avoid reference conflicts.
-// #[cfg(not(feature = "std"))]
-// #[no_mangle]
-// pub extern "C" fn _fini() -> ! {
-//     loop {}
-// }
+// avoid reference conflicts. 
+// TODO: needed by non-native env, needs furnish..
+#[cfg(not(feature = "std"))]
+#[no_mangle]
+pub extern "C" fn _fini() -> ! {
+    loop {}
+}
 
 #[cfg(not(feature = "std"))]
 #[no_mangle]
